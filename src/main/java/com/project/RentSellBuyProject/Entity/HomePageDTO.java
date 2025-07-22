62ac977577a6bcba2d8e9c81d5160fdc0b49ff13
Junaid <junaidmomin2005>
2025-07-23 02:23:55 +0530

    home page backend

---
+++ src/main/java/com/project/RentSellBuyProject/Entity/HomePageDTO.java
@@ -0,0 +1,12 @@
+package com.project.RentSellBuyProject.Entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+
+@Data
+public class HomePageDTO {
+    private String imageUrl;
+    private String productName;
+    private Integer price;
+    private String type;
+}
